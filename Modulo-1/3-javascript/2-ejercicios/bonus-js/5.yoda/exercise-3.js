38a756dd21024141ee5912539873a7e67c62e26c
Bea-ro <b_a_ro@hotmail.com>
2023-03-06 09:50:39 +0100

    finish exercise 3 yoda

--- Modulo-1/3-javascript/2-ejercicios/bonus-js/5.yoda/exercise-3.js
+++ Modulo-1/3-javascript/2-ejercicios/bonus-js/5.yoda/exercise-3.js
@@ -1,44 +1,38 @@
 const body = document.querySelector('body')
 
-const getData = () => {
-   fetch('http://localhost:3000/orders')
-    .then((res) => res.json())
-    .then((orders) => {
-        orders.sort((a,b) => b.date.replaceAll('-','') - a.date.replaceAll('-',''));
-        })
-
-    fetch('http://localhost:3000/products')
-    .then((res) => res.json())
-    .then((products) => products)
-
-    printOrders()
-}
-
-const printOrders = (orders, products) => {
-
-    orders.map((order) => {
-
-    const productsOrdered = order.products;
-
-    const orderTemplate = `
+const getOrderTemplate = (order, products) =>
+    `
     <div>
-<p>Fecha pedido: ${order.date}</p>
+    <p>Fecha pedido: ${order.date}</p>
 
-${productsOrdered.forEach((product) => {
+    ${order.products
+        .map((product) => {
     const productData = products.find((p) => {
     return p.id === product.productId;
-});
+    });
       return `
           <p>ID: ${productData.id}</p>
           <p>Nombre: ${productData.name}</p>
           <p>Cantidad: ${product.quantity}</p>
-      `;
+      `
       })
-      .join('')}
+      .join('')
+    }
       </div>`
+
+      fetch('http://localhost:3000/orders')
+      .then((res) => res.json())
+      .then((orders) => {
+          orders.sort((a,b) => b.date.replaceAll('-','') - a.date.replaceAll('-',''));
+          console.log(orders)
+          
+      
+       fetch('http://localhost:3000/products')
+      .then((res) => res.json())
+      .then((products) => { console.log(products);
+  
+orders.forEach((order) => {
+body.innerHTML += getOrderTemplate(order, products);
 })
-
-body.innerHTML += orderTemplate;
-}
-
-getData()
+})
+})
